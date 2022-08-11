129f6603c5294e93fb6dd67c021df8df5ebf7794
EyerusalemT21 <108164485+EyerusalemT21@users.noreply.github.com>
2022-08-11 13:45:45 -0500

    Add files via upload

---
+++ word.js
@@ -0,0 +1,24 @@
+
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "entries",
+});
+
+module.exports = con;
+// con.connect(function (err) {
+//   if (err) throw err;
+// });
+// function getWord(query,callback){
+//     con.query("SELECT * FROM entries where word='"+query+"'", function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//         callback(result);
+//       });
+// }
+
+
+
